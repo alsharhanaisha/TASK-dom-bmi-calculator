df0cdfe515a73dc5f2ad686841a5a171b78df998
alsharhanaisha <alsharhanaisha@gmail.com>
2022-01-08 18:40:12 +0300

    Healthy Weight Categories Updated

--- index.js
+++ index.js
@@ -8,17 +8,46 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
-  height = height / 100;
-  result = weight / (height * height);
-  alert(result);
 
-  if (result < 18.5) {
-    alert("YOU ARE UNDER WEIGHT!!!!");
-  } else if (result >= 18.5 && result <= 24.9) {
-    alert("Healthy Weight");
-  } else if (result >= 25.0 && result <= 29.9) {
-    alert("Overwight");
-  } else if (result >= 30.0) {
-    alert("OBESSSSEEEEE!!!!");
+  // Calculating BMI & Defining BMI Ranges:
+  let bmi = weight / (height / 100) ** 2;
+  let bmiResult;
+  if (bmi < 18.5) {
+    bmiResult = "Underweight";
+  } else {
+    if (
+      (age >= 19 && age <= 24 && bmi >= 19 && bmi <= 24) ||
+      (age >= 25 && age <= 34 && bmi >= 20 && bmi <= 25) ||
+      (age >= 35 && age <= 44 && bmi >= 21 && bmi <= 26) ||
+      (age >= 45 && age <= 54 && bmi >= 22 && bmi <= 27) ||
+      (age >= 55 && age <= 64 && bmi >= 23 && bmi <= 28) ||
+      (age >= 65 && bmi >= 24 && bmi <= 29)
+    ) {
+      bmiResult = "Healthy Weight";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      bmiResult = "Overweight";
+    } else {
+      bmiResult = "Obesity";
+    }
   }
+
+  // Male vs Female Result:
+  let gender;
+  if (male) {
+    gender = "Male";
+  } else {
+    gender = "Female";
+  }
+
+  // Posting the Result:
+  alert(`
+  Based on the following data you entered:
+  Age: ${age}
+  Weight: ${weight}
+  Height: ${height}
+  Gender: ${gender}
+  
+  Your Body Mass Index is:
+  ${bmi}
+  Which means, you are in the ${bmiResult} range.`);
 }
